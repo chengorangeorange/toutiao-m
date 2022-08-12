9f224f55c6ae3c22f4d28b5c36ad71b72b2aa001
chengorangeorange <1426641263@QQ.com>
2022-08-12 10:48:54 +0800

    黑头条

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+      <category></category>
+      <newlist></newlist>
+    </div>
+</template>
+
+<script>
+import category from './components/category.vue'
+import newlist from './components/new-list.vue'
+export default {
+components:{
+  category,
+  newlist
+}
+}
+</script>
+
+<style>
+
+</style>
